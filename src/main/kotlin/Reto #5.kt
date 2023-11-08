c47d3c76804c6ded43ef716b4ca5686c832308bd
macfer79 <149889281+macfer79@users.noreply.github.com>
2023-11-08 20:54:04 +0100

    HOLA MUNDO

---
+++ src/main/kotlin/Reto #5.kt
@@ -0,0 +1,12 @@
+/* HOLA MUNDO
+ * Escribe un !Hola Mundo! en todos los lenguajes de programación que puedas.
+ * Seguro que hay algún lenguaje que te llama la atención y nunca has utilizado,
+ * o quizás quieres dar tus primeros pasos... ¡Pues este es el momento!
+ *
+ * A ver quién se atreve con uno de esos lenguajes que no solemos ver por ahí...
+ */
+
+fun main (){
+    println("¡¡Hola, Mundo!!")
+    println("Escrito en Kotlin")
+}
